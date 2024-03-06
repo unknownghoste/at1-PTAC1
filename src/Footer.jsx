d52c2c75b1abcfb211a9c5a6f7bfb4e3b4719bf2
unknownghoste <99215178+unknownghoste@users.noreply.github.com>
2024-03-05 21:48:56 -0400

    Add files via upload

---
+++ src/Footer.jsx
@@ -0,0 +1,13 @@
+export default function Footer(){
+    return(
+        <footer style={{
+            backgroundColor: "purple",
+            height:50,
+            marginTop: 550,
+        }}>
+            <h3>
+                Desenvolvido por Ana Dantas 
+            </h3>
+        </footer>
+    )
+}
