3ac08ec35fc70643026cabc9f0f43e2f51bd6dee
unknownghoste <99215178+unknownghoste@users.noreply.github.com>
2024-03-20 16:00:47 -0400

    last update

---
+++ src/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header(){
+    return(
+        <header>
+            <h1> TESTE </h1>
+        </header> 
+    )
+}
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,16 +1,35 @@
 import {useState} from "react";
 
 export default function Main(){
+  const [nome, setNome] = useState("");
+
   return(
     <Main>
       <input
       type="text"
-      name=""
-      id=""
+      name="nome-contato"
+      id="nome"
       onChange={(event)=> setNome(event.target.value)}
     />
 
     {nome}
+    </Main>
+  );
+}
+
+export default function Main(){
+  const [telefone, setTelefone] = useState("");
+
+  return(
+    <Main>
+      <input
+      type="number"
+      name="telefone-contato"
+      id="telefone"
+      onChange={(event)=> setNome(event.target.value)}
+    />
+
+    {telefone}
     </Main>
   );
 }
